2979fb3e93b7935cfbca322c22df99d6d378adc2
HONG-JUNG-WAN <hong43ok@gmail.com>
2025-11-29 14:51:27 +0900

    chore: 프로젝트 초기화 및 개발 환경 설정

---
+++ src/main/java/io/github/hongjungwan/blackbox/BlackboxLoggingSdkApplication.java
@@ -0,0 +1,13 @@
+package io.github.hongjungwan.blackbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackboxLoggingSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackboxLoggingSdkApplication.class, args);
+	}
+
+}
